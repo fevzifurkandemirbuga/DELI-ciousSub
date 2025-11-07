58378bc2ea06134d2956b516d74002e6cdc88d85
fevzifurkandemirbuga <fevzifd2023@gmail..com>
2025-11-07 11:55:33 -0500

    create Order class

--- src/main/java/com/pluralsight/model/Order.java
+++ src/main/java/com/pluralsight/model/Order.java
@@ -4,7 +4,52 @@ import java.util.ArrayList;
 
 public class Order {
     private ArrayList<Sandwich> sandwiches;
-    private ArrayList<String> drinks;
+    private ArrayList<Drink> drinks;
     private int chips;
+
+
+    public Order() {
+        this.sandwiches = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = 0;
+    }
+
+
+    public void addSandwich(Sandwich sandwich){
+        this.sandwiches.add(sandwich);
+    }
+
+    public void addDrink(Drink drink){
+        this.drinks.add(drink);
+    }
+
+    public void addChip(){
+        this.chips++;
+    }
+
+    public ArrayList<Sandwich> getSandwiches() {
+        return sandwiches;
+    }
+
+    public ArrayList<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    @Override
+    public String toString() {
+        return sandwiches.stream()
+                .map(Sandwich::toString)
+                .reduce("",(a,b)->a+b)  +
+                drinks.stream()
+                        .map(Drink::toString)
+                        .reduce("",(a,b)->a+b)  +
+                chips + "chip" + (chips>1 ? "s" : "") + "\n";
+    }
+
+
 
 }
