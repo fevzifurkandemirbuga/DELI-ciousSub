d69095ece660193dd15761eaca2c5b51c5e3adb1
fevzifurkandemirbuga <fevzifd2023@gmail..com>
2025-11-07 07:29:40 -0500

    initial commit

--- src/main/java/com/pluralsight/model/Order.java
+++ src/main/java/com/pluralsight/model/Order.java
@@ -1,4 +1,10 @@
 package com.pluralsight.model;
 
+import java.util.ArrayList;
+
 public class Order {
+    private ArrayList<Sandwich> sandwiches;
+    private ArrayList<String> drinks;
+    private int chips;
+
 }
