f0ccaea75800a88b2915ec7f04490e1308cb6dd9
fevzifurkandemirbuga <fevzifd2023@gmail..com>
2025-11-10 11:02:16 -0500

    create ReceiptWriter class

---
+++ src/main/java/com/pluralsight/fileRepository/ReceiptWriter.java
@@ -0,0 +1,40 @@
+package com.pluralsight.fileRepository;
+
+import com.pluralsight.model.Order;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReceiptWriter {
+
+
+    public  void saveReceipt(Order order){
+        String date= String.format("%d-%d-%d",LocalDate.now().getYear(),
+                LocalDate.now().getMonthValue(),LocalDate.now().getDayOfMonth());
+        String time= String.format("%d-%d-%d",LocalTime.now().getHour(),
+                LocalTime.now().getMinute(),LocalTime.now().getSecond()
+                );
+        String fileName=date+"-"+time+".txt";
+
+        try{
+            FileWriter fileWriter=new FileWriter(fileName,true);
+            BufferedWriter bw=new BufferedWriter(fileWriter);
+            bw.write(order.toString());
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
